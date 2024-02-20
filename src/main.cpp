5159c62026914fb6ca9a6f4e9426ec464416597b
Ralf Zeilstra <ralfzeils@gmail.com>
2024-02-20 20:00:25 +0100

    Played Around more with ImGui

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <GLFW/glfw3.h>
 
 #include "imgui.h"
+#include "imgui_internal.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
 
@@ -13,6 +14,40 @@ void ShowToolBoxWindow()
 	ImGui::Begin("Sample Window");
 	ImGui::Text("Hi");
 	ImGui::SetItemTooltip("Hello");
+
+	// Add a button
+	if (ImGui::Button("Click Me!"))
+	{
+		std::cout << "Button Clicked!" << std::endl;
+	}
+	ImGui::SetItemTooltip("This is a button");
+
+
+	// End the window
+	ImGui::End();
+}
+
+void ShowTextBoxWindow()
+{
+	ImGui::Begin("Text");
+
+	//ImGui::InputTextMultiline("Input Text");
+	ImGui::Text("Input Text");
+
+	static char text[1024 * 16] =
+		"/*\n"
+		" The Pentium F00F bug, shorthand for F0 0F C7 C8,\n"
+		" the hexadecimal encoding of one offending instruction,\n"
+		" more formally, the invalid operand with locked CMPXCHG8B\n"
+		" instruction bug, is a design flaw in the majority of\n"
+		" Intel Pentium, Pentium MMX, and Pentium OverDrive\n"
+		" processors (all in the P5 microarchitecture).\n"
+		"*/\n\n"
+		"label:\n"
+		"\tlock cmpxchg8b eax\n";
+
+	static ImGuiInputTextFlags flags = ImGuiInputTextFlags_AllowTabInput;
+	ImGui::InputTextMultiline("##source", text, IM_ARRAYSIZE(text), ImVec2(-FLT_MIN, ImGui::GetTextLineHeight() * 16), flags);
 
 	// Add a button
 	if (ImGui::Button("Click Me!"))
@@ -67,39 +102,153 @@ int main()
 	ImGui_ImplOpenGL3_Init();
 
 
+	bool showDemoWindow = false;
+
+
+
 	// render loop
 	while (!glfwWindowShouldClose(window))
 	{
 		bool show = true;
-		bool showDemoWindow = false;
+		
 		glfwPollEvents();
 
 		// Start the Dear ImGui frame
 		ImGui_ImplOpenGL3_NewFrame();
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
-		ImGui::DockSpaceOverViewport();
-		ImGui::ShowDemoWindow(); 
+		ImGuiID dockID = ImGui::DockSpaceOverViewport();
+
+		ImGui::BeginMainMenuBar();
+		if (ImGui::BeginMenu("Menu"))
+		{
+			if (ImGui::MenuItem("New")) {}
+			if (ImGui::MenuItem("Open", "Ctrl+O")) {}
+			if (ImGui::BeginMenu("Open Recent"))
+			{
+				ImGui::MenuItem("fish_hat.c");
+				ImGui::MenuItem("fish_hat.inl");
+				ImGui::MenuItem("fish_hat.h");
+				if (ImGui::BeginMenu("More.."))
+				{
+					ImGui::MenuItem("Hello");
+					ImGui::MenuItem("Sailor");
+					if (ImGui::BeginMenu("Recurse.."))
+					{
+						//Function
+						ImGui::EndMenu();
+					}
+					ImGui::EndMenu();
+				}
+				ImGui::EndMenu();
+			}
+			if (ImGui::MenuItem("Save", "Ctrl+S")) {}
+			if (ImGui::MenuItem("Save As..")) {}
+
+			ImGui::Separator();
+
+			if (ImGui::BeginMenu("Options"))
+			{
+				static bool enabled = true;
+				ImGui::MenuItem("Enabled", "", &enabled);
+				ImGui::BeginChild("child", ImVec2(0, 60), ImGuiChildFlags_Border);
+				for (int i = 0; i < 10; i++)
+					ImGui::Text("Scrolling Text %d", i);
+				ImGui::EndChild();
+				static float f = 0.5f;
+				static int n = 0;
+				ImGui::SliderFloat("Value", &f, 0.0f, 1.0f);
+				ImGui::InputFloat("Input", &f, 0.1f);
+				ImGui::Combo("Combo", &n, "Yes\0No\0Maybe\0\0");
+				ImGui::EndMenu();
+			}
+
+
+			if (ImGui::BeginMenu("Colors"))
+			{
+				float sz = ImGui::GetTextLineHeight();
+				for (int i = 0; i < ImGuiCol_COUNT; i++)
+				{
+					const char* name = ImGui::GetStyleColorName((ImGuiCol)i);
+					ImVec2 p = ImGui::GetCursorScreenPos();
+					ImGui::GetWindowDrawList()->AddRectFilled(p, ImVec2(p.x + sz, p.y + sz), ImGui::GetColorU32((ImGuiCol)i));
+					ImGui::Dummy(ImVec2(sz, sz));
+					ImGui::SameLine();
+					ImGui::MenuItem(name);
+				}
+				ImGui::EndMenu();
+			}
+
+			ImGui::EndMenu();
+		}
+		ImGui::EndMainMenuBar();
+		
+
+
+
+
+
+		ImVec2 center = ImGui::GetMainViewport()->GetCenter();
+		ImGui::SetNextWindowPos(center, ImGuiCond_Appearing, ImVec2(0.5f, 0.5f));
+
+		
+
+		
+
+
+
+
+
+
 		ShowToolBoxWindow();
+		ShowTextBoxWindow();
 
-		ImGui::Begin("Options");
-		if(ImGui::Button("Show Demo Window")) 
+		if (showDemoWindow) { ImGui::ShowDemoWindow(); }
+
+
+
+		ImGui::Begin("Options", &show);
+
+		if(ImGui::Button("Toggle Demo Window"))
 		{
-			showDemoWindow = true;
+			showDemoWindow = !showDemoWindow;
+		}
+		ImGui::SetItemTooltip("Shows the demo window from ImGui for documentation and debug.");
+
+
+		if (ImGui::Button("Open Popup"))
+		{
+			ImGui::OpenPopup("Delete?");
+		}
+		if (ImGui::BeginPopupModal("Delete?", NULL, ImGuiWindowFlags_AlwaysAutoResize))
+		{
+			ImGui::Text("All those beautiful files will be deleted.\nThis operation cannot be undone!");
+			ImGui::Separator();
+
+			//static int unused_i = 0;
+			//ImGui::Combo("Combo", &unused_i, "Delete\0Delete harder\0");
+
+			static bool dont_ask_me_next_time = false;
+			ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(0, 0));
+			ImGui::Checkbox("Don't ask me next time", &dont_ask_me_next_time);
+			ImGui::PopStyleVar();
+
+			if (ImGui::Button("OK", ImVec2(120, 0))) { ImGui::CloseCurrentPopup(); }
+			ImGui::SetItemDefaultFocus();
+			ImGui::SameLine();
+			if (ImGui::Button("Cancel", ImVec2(120, 0))) { ImGui::CloseCurrentPopup(); }
+			ImGui::EndPopup();
 		}
 
-
-		if(showDemoWindow)
-		{
-			ImGui::ShowDemoWindow();
-		}
 		ImGui::End();
 
+
+
+		// ------------------------------------------- Rendering Stuff -------------------------------------------
 
 		// rendering commands
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-		
 
 		// Rendering
 		// (Your code clears your framebuffer, renders your other stuff etc.)
